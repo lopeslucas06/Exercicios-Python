0d3c348949f3156ba840b86b9a2788358e984af3
Lucas Lopes <lopeslucas200106@gmail.com>
2021-02-18 15:00:08 -0300

    ex094
    O programa deverá ler nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma lista. No final, deverá printar na tela, quantas pessoas foram cadastradas, a média de idade, uma lista com as mulheres, uma lista de pessoas com idade acima da média.
    
    Obs:
    
    O programa ainda não está adicionando as pessoas com idade acima da média.

---
+++ ex094.py
@@ -0,0 +1,27 @@
+pessoa = {}
+pessoas = []
+soma = cont = 0
+mulher = []
+maior = []
+while True:
+    pessoa['nome'] = str(input('Nome: '))    
+    pessoa['sexo'] = str(input('Sexo: [M/F] '))[0].upper().strip()
+    if pessoa['sexo'] in 'F':
+        mulher.append(pessoa['nome'])  
+    pessoa['idade'] = int(input('Idade: '))  
+    soma += pessoa['idade']  
+    cont += 1
+    pessoas.append(pessoa.copy())        
+    r = str(input('Deseja continuar? [S/N] '))[0].upper().strip()
+    if r == 'N':
+        break
+media = soma / cont   
+print(pessoas)
+print(f'O número de pessoas cadastradas foram {len(pessoas)}')
+print(f'A média das idades é de {media:.2f} anos!')
+print(f'As mulheres são {mulher}')
+
+
+    
+    
+
